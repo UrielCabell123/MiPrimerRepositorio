58921d1acb5ad90fc505124b7d12559fdb3ed665
Uriel Cabello Sosa <cabellososau19@gmail.com>
2024-04-28 02:11:37 -0600

    En este repositorio se creo una plantilla utilizando clases, positions, z-index, inicio del framework

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cliente/css/estilos.css">
+    <title>Index</title>
+    
+</head>
+
+<body class="exedente">
+<img src="cliente/imagenes/dulces.jpg" class="fondo" alt="">
+    
+        <div class="row" id="contenedor-index">
+             
+             <div class="card">
+                <div class="imagen-c"></div>
+             <h1 class="letra">Hola Soy uriel</h1>
+             <h3 class="descripcion">vivo en san martin</h3>
+             <button type="button" class="boton-carrito">🛒</button>
+            </div>
+
+
+        </div>
+    
+</body>
+</html>
